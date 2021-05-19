df007b7ac4c1850aec1bf82ecd0815c9c4de02ba
gallottohugo <gallottohugo@gmail.com>
2021-05-19 11:34:11 -0300

    Supported currencies

--- bitcoin_wallet.py
+++ bitcoin_wallet.py
@@ -108,3 +108,33 @@ class BitcoinWallet():
 
 		private_key = wif_to_key(private_key_wif)
 		return private_key
+
+	@staticmethod
+	def supported_currencies():
+		return {
+			"satoshi": "Satoshi",
+			"ubtc": "Microbitcoin",
+			"mbtc": "Millibitcoin",
+			"btc": "Bitcoin",
+			"usd": "United States Dollar",
+			"eur": "Eurozone Euro",
+			"gbp": "Pound Sterling",
+			"jpy": "Japanese Yen",
+			"cny": "Chinese Yuan",
+			"cad": "Canadian Dollar",
+			"aud": "Australian Dollar",
+			"nzd": "New Zealand Dollar",
+			"rub": "Russian Ruble",
+			"brl": "Brazilian Real",
+			"chf": "Swiss Franc",
+			"sek": "Swedish Krona",
+			"dkk": "Danish Krone",
+			"isk": "Icelandic Krona",
+			"pln": "Polish Zloty",
+			"hkd": "Hong Kong Dollar",
+			"krw": "South Korean Won",
+			"sgd": "Singapore Dollar",
+			"thb": "Thai Baht",
+			"twd": "New Taiwan Dollar",
+			"clp": "Chilean Peso",
+		}
